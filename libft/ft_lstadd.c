cbd94506ab9f470f6030dab5ce13e483274cb3c0
Artem KOROL <akorol@e1r5p13.unit.ua>
2018-12-28 20:21:45 +0200

    hello

--- libft/ft_lstadd.c
+++ libft/ft_lstadd.c
@@ -1,34 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akorol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/19 14:00:05 by akorol            #+#    #+#             */
-/*   Updated: 2018/11/23 18:42:42 by akorol           ###   ########.fr       */
+/*   Created: 2018/11/19 15:35:58 by akorol            #+#    #+#             */
+/*   Updated: 2018/12/24 11:54:54 by akorol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void const *content, size_t content_size)
+void	ft_lstadd(t_list **alst, t_list *new)
 {
-	t_list	*newlink;
-
-	if (!(newlink = (t_list*)malloc(sizeof(t_list))))
-		return (NULL);
-	if (content == NULL)
+	if (alst != NULL && new != NULL)
 	{
-		newlink->content = NULL;
-		newlink->content_size = 0;
+		new->next = *alst;
+		*alst = new;
 	}
-	else
-	{
-		newlink->content = malloc(content_size);
-		ft_memcpy(newlink->content, content, content_size);
-		newlink->content_size = content_size;
-	}
-	newlink->next = NULL;
-	return (newlink);
 }
